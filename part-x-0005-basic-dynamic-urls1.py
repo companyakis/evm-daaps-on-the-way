1414813544a1f5bcd575bfe5ca2584c4d758e8ec
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-09-03 21:46:10 +0300

    Create part-x-0005-basic-dynamic-urls1.py

---
+++ part-x-0005-basic-dynamic-urls1.py
@@ -0,0 +1,15 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Main Page</h1>"
+
+@app.route("/employees/<int:emp_id>/<emp_name>")
+def emp_info(emp_id, emp_name):
+    return f"<h1>Employee name: {emp_name} and ID: {emp_id}</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
