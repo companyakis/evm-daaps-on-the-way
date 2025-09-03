38dbe5f4859486340db4ca617ca91af5881f9f45
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-09-03 21:52:46 +0300

    Update part-x-0005-basic-dynamic-urls1.py

--- part-x-0005-basic-dynamic-urls1.py
+++ part-x-0005-basic-dynamic-urls1.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route("/employees/<int:emp_id>/<emp_name>")
 def emp_info(emp_id, emp_name):
-    return f"<h1>Employee name: {emp_name} and ID: {emp_id}</h1>"
+    return f"<h1>Employee name: {emp_name.title()} and ID: {emp_id}</h1>"
 
 
 if __name__ == "__main__":
